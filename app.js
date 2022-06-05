e12e0e82b357b6470db7d4c6f1286d8b47f91ff0
lestercoyoyjr <lester_cch@hotmail.com>
2022-06-05 14:46:05 -0600

    DB Connection
    How to perform our connection

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ var mysql = require('mysql');
 
 var app = express();
 
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'articulosdb'
+});
+
+connection.connect(function(error){
+    if(error){
+        throw error;
+    } else {
+        console.log('Conexion exitosa a la base de datos!');
+    }
+});
+
 // rutas
 app.get('/', function(req, res){
     res.send('Ruta INICIO');
