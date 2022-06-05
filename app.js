588e6119815285bfc701a68fdcceb61aa2c21984
lestercoyoyjr <lester_cch@hotmail.com>
2022-06-05 15:10:04 -0600

    Get
    Get articulos
    Get articulo

--- app.js
+++ app.js
@@ -23,6 +23,32 @@ app.get('/', function(req, res){
     res.send('Ruta INICIO');
 })
 
+// Todos los articulos
+app.get('/api/articulos',(req,res)=>{
+    connection.query('SELECT * FROM articulos', (error, filas)=>{
+        if (error) {
+            throw error;
+        } else {
+            res.send(filas);
+        }
+    })
+});
+
+// solo un articulo
+app.get('/api/articulos/:id',(req,res)=>{
+    connection.query('SELECT * FROM articulos WHERE id=?', [req.params.id], (error, fila)=>{
+        if (error) {
+            throw error;
+        } else {
+            res.send(fila);
+            /*
+                Si quisieramos un solo campo del registro
+                res.send(fila[0].descripcion);
+            */ 
+        }
+    })
+});
+
 // in case port is occupied
 const puerto = process.env.PUERTO;
 
